88161b6679fc68dfa25acd4b13437c9270410412
MoizAsif <moiz.asif@xynotech.com>
2023-02-02 15:57:45 +0500

    Adding RDS

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ My First codebase from terraform
 # Useful Links
 
 https://www.sammeechward.com/terraform-vpc-subnets-ec2-and-more
+
+https://fitdevops.in/create-rds-instances-using-terraform/
--- main.tf
+++ main.tf
@@ -4,16 +4,16 @@ provider "aws" {
 
 # Create Instance
 
-resource "aws_instance" "app_server" {
-  ami             = var.image_id
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.public_subnet.id
-  security_groups = ["${aws_security_group.moiz_web_sg.id}"]
+# resource "aws_instance" "app_server" {
+#   ami             = var.image_id
+#   instance_type   = var.instance_type
+#   subnet_id       = aws_subnet.public_subnet.id
+#   security_groups = ["${aws_security_group.moiz_web_sg.id}"]
 
-  tags = {
-    Name = "VarTest"
-  }
-}
+#   tags = {
+#     Name = "VarTest"
+#   }
+# }
 
 
 
@@ -51,13 +51,13 @@ resource "aws_subnet" "private_subnet" {
 
 # Create Internetgateway
 
-resource "aws_internet_gateway" "test_igw" {
-  vpc_id = aws_vpc.test_vpc.id
+# resource "aws_internet_gateway" "test_igw" {
+#   vpc_id = aws_vpc.test_vpc.id
 
-  tags = {
-    Name = "Internet Gateway"
-  }
-}
+#   tags = {
+#     Name = "Internet Gateway"
+#   }
+# }
 
 
 
@@ -65,29 +65,29 @@ resource "aws_internet_gateway" "test_igw" {
 
 # Create Security Group
 
-resource "aws_security_group" "moiz_web_sg" {
-  name   = "HTTP and SSH"
-  vpc_id = aws_vpc.test_vpc.id
+# resource "aws_security_group" "moiz_web_sg" {
+#   name   = "HTTP and SSH"
+#   vpc_id = aws_vpc.test_vpc.id
 
-  dynamic "ingress" {
-    for_each = var.i_ports
-    iterator = i_port
-    content {
-      description = "SSH HTTP, and HTTPS"
-      from_port   = i_port.value
-      to_port     = i_port.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
+#   dynamic "ingress" {
+#     for_each = var.i_ports
+#     iterator = i_port
+#     content {
+#       description = "SSH HTTP, and HTTPS"
+#       from_port   = i_port.value
+#       to_port     = i_port.value
+#       protocol    = "tcp"
+#       cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = -1
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
 
@@ -95,26 +95,50 @@ resource "aws_security_group" "moiz_web_sg" {
 
 # Route Tables
 
-resource "aws_route_table" "moiz_public_rt" {
-  vpc_id = aws_vpc.test_vpc.id
+# resource "aws_route_table" "moiz_public_rt" {
+#   vpc_id = aws_vpc.test_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.test_igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.test_igw.id
+#   }
 
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.test_igw.id
-  }
+#   route {
+#     ipv6_cidr_block = "::/0"
+#     gateway_id      = aws_internet_gateway.test_igw.id
+#   }
 
-  tags = {
-    Name = "Test Public Route Table"
-  }
+#   tags = {
+#     Name = "Test Public Route Table"
+#   }
+# }
+
+
+# resource "aws_route_table_association" "public_rt" {
+#   subnet_id      = aws_subnet.public_subnet.id
+#   route_table_id = aws_route_table.moiz_public_rt.id
+# }
+
+
+
+#Creating RDS
+
+resource "aws_db_instance" "test" {
+  allocated_storage    = 20
+  identifier           = "test123"
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = "admin"
+  password             = "admin"
+  db_subnet_group_name = aws_db_subnet_group.test-db-subnet.name
+  # parameter_group_name = "default.mysql5.7"
 }
 
+#Creating RDS Subnet Group
 
-resource "aws_route_table_association" "public_rt" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.moiz_public_rt.id
+resource "aws_db_subnet_group" "test-db-subnet" {
+  name       = "db subnet group"
+  subnet_ids = ["${aws_subnet.public_subnet.id}", "${aws_subnet.private_subnet.id}"]
 }
